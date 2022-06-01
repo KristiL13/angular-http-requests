083cd3aa197fa10c0b01de86f6951dfe4987ae4a
Kristi Laur <kristi@heisi.eu>
2022-06-01 16:17:06 +0300

    Move requests and data transformation into a Service + communicating with component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -12,27 +12,17 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private postsService: PostsService) {}
 
   ngOnInit() {
     this.fetchPosts();
   }
 
   onCreatePost(postData: Post) {
-    // Send Http request
-    console.log(postData);
-    // http requests are wrapped by Observables. You need to subscribe to it.
-    // Muidu Angular arvab, et keegi pole vastusest huvitatud ja ei saada isegi päringut.
-    // Kõigi päringute puhul saab määrata vastuse Body tüübi ka.
-    // See annab ka edasi vastuse töötlemise juures parema autocompletioni.
-    this.http.post<{ name: string }>(
-      // Siin .json on API aadressi lõpus, lihtsalt kuna Firebaseil endal on nii vaja
-      'https://ng-complete-guide-408bf-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
-      // Siin Angulari HttpClient teeb meie JS objektist ise JSONi ja saadab JSON datat.
-      postData
-    ).subscribe(responseData => {
-      console.log(responseData);
-    });
+    // If the component doesn't care about the response,
+    // then there is no reason to subscribe to it in the component.
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
@@ -44,30 +34,13 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    // Keep the UI and component/template related stuff in the component.
+    // If I care about the response and the response status, then
+    // having it split between service and component is useful.
     this.isFetching = true;
-    // get has only one argument, because there is no request Body.
-    // Peab subscribeima ikka ka, muidu päringut ei saadeta.
-    // Selline [key: string] väljend [] sees tähendab, et see on mingi
-    // random string, millel pole nime.
-    this.http
-      // get järel <> sees saab ette anda vastuse Body tüübi TSle.
-      .get<{ [key: string]: Post }>('https://ng-complete-guide-408bf-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
-      // map tagastab ka Observable-i, seega saame subscribeida all pool.
-      .pipe(map(responseData => {
-        const postsArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            // Teeme uue objekti saadud data käesolevale key-le vastavast objektist
-            // ning saame ka lisada lisa key-value paare.
-            postsArray.push({...responseData[key], id: key});
-          }
-        }
-        return postsArray;
-      }))
-      .subscribe(posts => {
-        // console.log(posts);
-        this.isFetching = false;
-        this.loadedPosts = posts;
-      });
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
   }
 }
---
+++ src/app/posts.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
+
+@Injectable({
+  providedIn: 'root' // moodsam provideimise lahendus
+})
+export class PostsService {
+
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(title: string, content: string) {
+    // If the component doesn't care about the response,
+    // then there is no reason to subscribe to it in the component.
+    const postData: Post = {title: title, content: content}
+    // here i will send my post
+    // http requests are wrapped by Observables. You need to subscribe to it.
+    // Muidu Angular arvab, et keegi pole vastusest huvitatud ja ei saada isegi päringut.
+    // Kõigi päringute puhul saab määrata vastuse Body tüübi ka.
+    // See annab ka edasi vastuse töötlemise juures parema autocompletioni.
+
+    this.http.post<{ name: string }>(
+      // Siin .json on API aadressi lõpus, lihtsalt kuna Firebaseil endal on nii vaja
+      'https://ng-complete-guide-408bf-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+      // Siin Angulari HttpClient teeb meie JS objektist ise JSONi ja saadab JSON datat.
+      postData
+    ).subscribe(responseData => {
+      console.log(responseData);
+    });
+  }
+
+  fetchPosts() {
+    // here i will send my request
+    // get has only one argument, because there is no request Body.
+    // Peab subscribeima ikka ka, muidu päringut ei saadeta.
+    // Selline [key: string] väljend [] sees tähendab, et see on mingi
+    // random string, millel pole nime.
+    // Set up the Observable in the service, but subscribe in the component.
+    return this.http
+      // get järel <> sees saab ette anda vastuse Body tüübi TSle.
+      .get<{ [key: string]: Post }>('https://ng-complete-guide-408bf-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+      // map tagastab ka Observable-i, seega saame subscribeida all pool.
+      .pipe(map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            // Teeme uue objekti saadud data käesolevale key-le vastavast objektist
+            // ning saame ka lisada lisa key-value paare.
+            postsArray.push({...responseData[key], id: key});
+          }
+        }
+        return postsArray;
+      }));
+  }
+}
